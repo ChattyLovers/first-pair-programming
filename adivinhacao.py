9b4014d63621d3f7f4f24d5607d79cdd38e23284
Marcia W. Bastos <marciaw.bastos@gmail.com>
2020-04-15 21:05:33 -0300

    adivinhacao.py

---
+++ adivinhacao.py
@@ -0,0 +1,13 @@
+import random
+
+frutas = ['banana', 'jabuticaba', 'pitanga', 'mirtilo', 'morango', 'abacaxi', 'cereja']
+
+print('Bem vindo ao jogo de adivinhação')
+
+fruta = random.choice(frutas)
+acabou = False
+
+while not acabou:
+    print(" _ " * len(fruta))
+    letra = input('Digite uma letra: ')
+    print(letra in fruta)
